89452160ea1ea83f4a7203e268a60a8152b722c2
Rudra Saraswat <rs2009@ubuntu.com>
2023-01-20 11:37:31 +0530

    remove debian packaging

--- blend-pkgmngr-path.sh
+++
@@ -1,9 +0,0 @@
-# shellcheck shell=sh
-
-# Expand $PATH to include the directory where blend's package manager shortcuts
-# are located.
-blend_pkgmanager_bin_path="/blend/pkgmanagers"
-if [ -n "${PATH##*${blend_pkgmanager_bin_path}}" ] && [ -n "${PATH##*${blend_pkgmanager_bin_path}:*}" ]; then
-    export PATH="${blend_pkgmanager_bin_path}:${PATH}"
-fi
-

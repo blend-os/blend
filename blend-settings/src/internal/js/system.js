87467b6105b5937f189264bc170822e38b762a9c
Rudra Saraswat <rs2009@ubuntu.com>
2023-04-21 20:03:28 +0530

    Add toggle for app grouping

--- blend-settings/src/internal/js/system.js
+++ blend-settings/src/internal/js/system.js
@@ -92,6 +92,13 @@ function check_state_creation() {
     }
 }
 
+function check_app_grouping() {
+    if (require('fs').existsSync(`${require('os').homedir()}/.config/categorize_apps_gnome_disable`)) {
+        document.getElementById('app-grouping-toggle').setAttribute('checked', '')
+    }
+}
+
+check_app_grouping()
 check_state_creation()
 check_rollback()
 
@@ -134,3 +141,48 @@ $('#automatic-state-toggle').on('change', () => {
         }
     }
 });
+
+$('#app-grouping-toggle').on('change', () => {
+    if (!document.getElementById('app-grouping-toggle').checked) {
+        let enable_autogrouping_worker = new Worker(
+            `data:text/javascript,
+            let s = require('child_process').spawnSync('rm', ['-f', '${require('os').homedir()}/.config/categorize_apps_gnome_disable']).status
+            if (s === 0) {
+                postMessage('success')
+            } else {
+                postMessage('failure')
+            }
+            `
+        )
+        enable_autogrouping_worker.onmessage = e => {
+            if (e.data == 'success') {
+                document.getElementById('app-grouping-toggle').checked = false
+            } else {
+                document.getElementById('app-grouping-toggle').checked = true
+            }
+        }
+    } else {
+        let disable_autogrouping_worker = new Worker(
+            `data:text/javascript,
+            require('child_process').spawnSync('mkdir', ['-p', '${require('os').homedir()}/.config']).status
+            let s = require('child_process').spawnSync('touch', ['${require('os').homedir()}/.config/categorize_apps_gnome_disable']).status
+            if (s === 0) {
+                postMessage('success')
+            } else {
+                postMessage('failure')
+            }
+            `
+        )
+        disable_autogrouping_worker.onmessage = e => {
+            if (e.data == 'success') {
+                document.getElementById('app-grouping-toggle').checked = true
+            } else {
+                document.getElementById('app-grouping-toggle').checked = false
+            }
+        }
+    }
+});
+
+if (require('process').env.XDG_CURRENT_DESKTOP.includes('GNOME')) {
+    $('#app-grouping-item').removeClass('d-none')
+}

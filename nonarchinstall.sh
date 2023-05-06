76f46f7e1aab34fdd494fb97fc490fab7f1bbd5a
lilkidsuave <66171643+lilkidsuave@users.noreply.github.com>
2023-05-06 18:08:26 -0400

    Add files via upload
    the script yall been waiting for

---
+++ nonarchinstall.sh
@@ -0,0 +1,36 @@
+DESTINATION=/usr
+FOLDERNAME=usr
+if ! [ $(id -u) = 0 ]; then
+   echo "The script need to be run as root." >&2
+   exit 1
+fi
+
+if [ $SUDO_USER ]; then
+    real_user=$SUDO_USER
+else
+    real_user=$(whoami)
+fi
+
+    curl -s https://api.github.com/repos/electron/electron/releases/latest \
+    | grep "browser_download_url.*electron-.*-linux-x64.zip" \
+    | cut -d : -f 2,3 \
+    | tr -d \" \
+    | wget -qi -
+    unzip -q -o '*electron*.zip' -d electron22
+    sudo -u ${real_user} git clone https://github.com/blend-os/blend-pkg
+    sudo cp -R electron22 ${DESTINATION}/lib/
+    cd blend-pkg
+    sudo -u ${real_user} makepkg --nodeps
+    for f in *.tar.gz; do \
+		tar xzf "$f"; \
+    done 
+    mv usr -Tf blendWorkspace 
+    mv blendWorkspace -Tf ${FOLDERNAME} 
+    sudo cp -RT ${FOLDERNAME} ${DESTINATION} 
+    sudo cp -RT etc /etc
+    cd /etc/profile.d 
+    chmod +x blend.sh 
+    sudo -u ${real_user} ./blend.sh
+    cd ${DESTINATION}/lib/systemd/user 
+    ls
+    sudo -u ${real_user} systemctl --user enable blend-files.service
